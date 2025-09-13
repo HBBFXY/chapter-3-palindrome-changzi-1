8901597a1b0b87c8011552dfe7d0ded4609d58fd
changzi-1 <2656748923@qq.com>
2025-09-13 23:19:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-num=input("请输入一个5位数字:")
-if len(num) !=5 or not
-num.isdigit():
+input_number=input("请输入一个5位数字:")
+if len(input_number) !=5 or not
+input_number.isdigit():
   print("请输入有效的5位数字!")
 else:
-  if num == num[::-1]:
+  if input_number == input_number[::-1]:
     print("是回文数")
   else:
     print("不是回文数")
