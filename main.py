38d957f09b0bfeddefc8ca1e758b6d9edfe5899e
changzi-1 <2656748923@qq.com>
2025-09-13 23:27:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-input_number=input("请输入一个5位数字:")
+While True:
+input_number=input("请输入一个5位数字(输入e退出):")
+if input_number()=='e':
+  print("程序已退出")
+  break
 if len(input_number) !=5 or not
 input_number.isdigit():
-  print("请输入有效的5位数字!")
+  print("输入错误,请输入有效的5位数字:")
+  continue
 else:
   if input_number == input_number[::-1]:
     print("是回文数")
