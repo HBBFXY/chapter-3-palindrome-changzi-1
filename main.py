5a91460e1ffddfa674f7117c61c52ea91a5049eb
changzi-1 <2656748923@qq.com>
2025-09-14 14:31:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,8 @@
-def is_palindrome():
-    while True:
-        num = input("请输入一个5位数字（输入e退出）：")
-        if num.lower() == 'e':
-            print("程序已退出。")
-            break
-        if len(num) != 5:
-            print("输入错误，请确保输入一个5位数字。")
-            continue
-        if not num.isdigit():
-            print("输入错误，请确保输入的是数字。")
-            continue
-        if num == num[::-1]:
-            print("是回文数。")
-        else:
-            print("不是回文数。")
-
-is_palindrome()
+n = input('请输入一个5位数字: ')
+if len(n)!=5 or not n.isdigit():
+    print('输入错误: 请输入5位数字')
+else:
+    if n==n[::-1]:
+        print('是回文数')
+    else:
+        print('不是回文数')
